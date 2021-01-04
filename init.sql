e13379cb1601b38fff76d4d1954c21b00f5eabdf
ermisenes <enes.ermis1@gmail.com>
2021-01-04 16:45:25 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE TABLE public.patients
 (    
   id character varying(50) NOT NULL,    
   Personname character varying(200) NOT NULL,    
-  address character varying(500),    
+  Personaddress character varying(500),    
   city character varying(100),    
   age numeric NOT NULL,    
   gender character varying(10),    
