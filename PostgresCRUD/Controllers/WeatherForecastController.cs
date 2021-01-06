13f3a3904df49fd0abbfd9acba08308e65bcba41
ermisenes <enes.ermis1@gmail.com>
2021-01-06 12:00:15 +0300

    Update WeatherForecastController.cs

--- PostgresCRUD/Controllers/WeatherForecastController.cs
+++ PostgresCRUD/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace PostgresCRUD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-         "Donma"
+       "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
